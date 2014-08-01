fa32f2ed3d9b6bcade503cbf707d337f748dc476
Joe Gibbs Politz <joe.politz@gmail.com>
2014-07-31 22:02:58 -0400

    manually add books

--- report/bib.rkt
+++ report/bib.rkt
@@ -74,7 +74,64 @@
     #:date "2002"
     #:location
       (proceedings-location  "ACM SIGCSE Conference on Innovation and Technology in Computer Science Education")))
+
+(define punished-by-rewards
+  (make-bib
+    #:title "Punished By Rewards"
+    #:author "Alfie Kohn"
+    #:date "1999"
+    #:location
+      (book-location #:publisher "Houghton Mifflin Company")))
+
+(define geert
+  (make-bib
+    #:title "Cultures and Organizations: Software of the Mind"
+    #:author
+      (authors
+        (author-name "Geert" "Hofstedet")
+        (author-name "Gert Jan" "Hofstede"))
+    #:date "2005"
+    #:location
+      (book-location #:publisher "McGraw-Hill")))
+
+(define psych-programming
+  (make-bib
+    #:author "Gerald M. Weinberg"
+    #:title "The Psychology of Computer Programming"
+    #:date "1971"
+    #:location
+      (book-location #:publisher "Van Nostrand Reinhold")))
   
+(define bloom
+  (make-bib
+    #:title "Taxonomy of Educational Objectives: The Classification of Educational Goals. Handbook I: Cognitive Domain"
+    #:author
+      (authors (author-name "B. S." "Bloom")
+               (author-name "D. R." "Krathwohl"))
+    #:date "1956"
+    #:location
+      (book-location #:publisher "Longmans")))
+
+(define mazur
+  (make-bib
+    #:title "Peer Instruction: A User's Manual"
+    #:author
+      (authors (author-name "Eric" "Mazur"))
+    #:date "1996"
+    #:location
+      (book-location #:publisher "Addison-Wesley")))
+
+(define smartbear
+  (make-bib
+    #:title "Best Kept Secrets of Peer Code Review"
+    #:author
+      (authors (author-name "Jason" "Cohen")
+               (author-name "Steven" "Teleki")
+               (author-name "Eric" "Brown"))
+    #:date "2013"
+    #:location
+      (book-location #:publisher "SmartBear Software")))
+
 
 (define special-keys
   (hash "deci-self-determination" deci
@@ -83,6 +140,12 @@
         "adrw:2000:worked-examples" renkl-1 
         "r:1997:self-explanations" renkl-2 
         "mk:2002:peer-mentor" miller
+        "w:psychology-computer-programming" psych-programming
+        "ghgjh:cult-org" geert
+        "punished-by-rewards" punished-by-rewards
+        "smartbear-code-review" smartbear
+        "mazur-peer-isntr-book" mazur
+        "bloom-taxonomy" bloom
         ))
           
 (define-cite autobib-cite _ generate-bib #:style number-style)
